db4e9a7b1ffaabddd31f1e7549801217fbcd802a
kunda <kunda@LAPTOP-NJOTD0LD>
2022-03-12 19:49:38 +0530

    code commit for arralist

--- uberRide/src/main/java/com/uber/ride/controller/UserLogin.java
+++ uberRide/src/main/java/com/uber/ride/controller/UserLogin.java
@@ -1,10 +1,23 @@
 package com.uber.ride.controller;
 
+import java.util.ArrayList;
+
 public class UserLogin {
 	
 	
 
 	public static void main(String[] args) {
-		System.out.println("kundan here..");
+		System.out.println("starting ..");
+		
+		ArrayList<Object> arraylist = new ArrayList<>();
+		arraylist.add("kundan");
+		arraylist.add(10);
+		arraylist.add(10.89);
+		arraylist.add(new ArrayList<String>());
+		
+		for(Object object:arraylist) {
+		
+			System.out.println("Arralist:"+object);
+		}
 	}
 }
